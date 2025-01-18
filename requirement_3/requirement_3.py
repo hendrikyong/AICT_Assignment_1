522fe4ebd492c516e26015d5dff12da2a3feec2e
Hendrik Yong <hendrikyong1205@gmail.com>
2025-01-18 15:29:52 +0800

    added variables and dataset

--- requirement_3/requirement_3.py
+++ requirement_3/requirement_3.py
@@ -16,14 +16,21 @@ Deliverables:
 •	Analysis and discussion of the model’s accuracy and limitations.
 '''
 
-from pgmpy.models import BayesianModel
+'''
+Vehicle Count (VC): The number of vehicles passing a specific point per unit time.
+Average Vehicle Speed (AS): Average speed of vehicles in a segment of the road.
+Road Surface Conditions (RC): Dry, wet
+Weather (W): Sunny, Rainy, Foggy
+Time of Day (T): Morning, Afternoon, Evening
+Day of the Week (D): Weekday, Weekend
+Road Incidents (RI): None, Minor, Major
+Historical Congestion Level (H): Low, Medium, High
+'''
+
+from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
-model = BayesianModel()
+from pgmpy.inference import VariableElimination
+import pandas as pd
 
-
-#defining conditional probability distributions 
-#need to input probability when decided 
-cpd_weather = TabularCPD(variable='Weather', variable_card=3, values=[[], [], []])
-cpd_time_of_day = TabularCPD(variable='Time of Day', variable_card=3, values=[[], [], []])
-cpd_historical_congestion_level = TabularCPD(variable='Historical Congestion Level', variable_card=3, values=[[], [], []])
-cpd_current_congestion_level = TabularCPD(variable='Current Congestion Level', variable_card=3, values=[[], [], []])
+df = pd.read_csv('requirement_3/data.csv')
+# print(df.head())
